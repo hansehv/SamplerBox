3773818d63cb1414edb340631459c8707ccd20ca
hansehv <30558081+hansehv@users.noreply.github.com>
2018-07-31 22:45:08 +0200

    Made compile warning free

--- filters/make.sh
+++ filters/make.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo perform next command from command line:
 echo gcc -Wall -Wextra -O -ansi -pedantic -fpermissive -shared interface.cpp -o interface.so
+# test compile separate modules: gcc -Wall -Wextra -O -ansi -pedantic -fpermissive -shared module.cpp -lpthread -lm
+# no -o necessary, you will end up with an a.so
