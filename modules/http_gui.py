8546ab90981127c237e89ecbaaf99a23f409ddef
hansehv <30558081+hansehv@users.noreply.github.com>
2019-02-10 23:45:07 +0100

    Regression fix

--- modules/http_gui.py
+++ modules/http_gui.py
@@ -8,7 +8,7 @@ import threading
 import BaseHTTPServer,urllib, mimetypes
 from urlparse import urlparse,parse_qs
 import os,shutil,subprocess
-import gv,LFO,arp
+import gv,LFO
 notesymbol=["C","C&#9839;","D","E&#9837;","E","F","F&#9839;","G","G&#9839;","A","B&#9837;","B","FX"]  # 0 in scalechord table, also used in loadsamples(!)
 HTTP_PORT = 80
 
@@ -40,7 +40,8 @@ class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_header("Content-Length", str(fs[6]))
             self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
             self.end_headers()
-            shutil.copyfileobj(f, self.wfile)
+            shutil.copyfileobj(f, self.wfile)  - Introduced override on midiCC mapping per voice
+  - Cosmetic changes and bugfixes
             f.close()
         
     def do_HEAD(self):
@@ -55,7 +56,8 @@ class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         field_data = self.rfile.read(length)
         fields=parse_qs(field_data)
         #print fields
-        
+          - Introduced override on midiCC mapping per voice
+  - Cosmetic changes and bugfixes
         if "SB_RenewMedia" in fields:
             if fields["SB_RenewMedia"][0] == "Yes":
                 gv.basename="None"
@@ -66,7 +68,8 @@ class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             if gv.PRESET!=inval:
                 gv.PRESET=inval;
                 self.LoadSamples()
-                return
+                return  - Introduced override on midiCC mapping per voice
+  - Cosmetic changes and bugfixes
         if "SB_DefinitionTxt" in fields:
             if gv.DefinitionTxt != fields["SB_DefinitionTxt"][0]:
                 gv.DefinitionTxt = fields["SB_DefinitionTxt"][0]
@@ -84,7 +87,8 @@ class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         if "SB_MidiVolume"  in fields: gv.volumeCC          =float(fields["SB_MidiVolume"][0])/100
         if "SB_Gain"        in fields: gv.globalgain        =float(fields["SB_Gain"][0])/100
         if "SB_Pitchrange"  in fields: gv.pitchnotes        =int(fields["SB_Pitchrange"][0])*2
-        if "SB_Voice"       in fields:
+        if "SB_Voice"       in fields:  - Introduced override on midiCC mapping per voice
+  - Cosmetic changes and bugfixes
             if gv.voicelist[0][0]==0:   i=1
             else:   i=0
             gv.MC[gv.getindex(gv.VOICES,gv.MC)][2](int(fields["SB_Voice"][0])+i,0)
