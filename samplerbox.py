b3e1d6b01a5e7529da1fd0192a7929467769396f
TheNothingMan <s.skr.k123@gmail.com>
2019-04-23 22:30:09 +0200

    Master (#23)
    * Bugfix: Global Transpose and buttons
    
    Remove global transpose from the list of button functions to match the
    removal of global transpose (obsoleted feature)
    
    * Bugfix: Button_display()
    
    Remove globaltranspose from Button_display()
    
    * Fix: global sample mode not recognized
    
    When loading a new sample set, compare the global sample mode flag to
    the actual default sample mode instead of -2, in case Keyb was
    overwritten in the boot/configuration.txt
    
    * Revert "Fix: global sample mode not recognized"
    
    This reverts commit 7bbda395aafd76bcd41a5007d8bfcb85c88a1b26.
    
    * Initial support for SH1106 OLED displays
    
    Add a module for SH1106 displays and the necessary minor changes to
    samplerbox.py and configuration.txt
    
    * Refactor modular OLED module
    
    Change the OLED module to be generic and add a config option for
    different drivers than SH1106 to prepair support of all OLED drivers
    that are supported by Luma.OLED.
    
    * Rework and cleanup of OLED module
    
    Moved all calls to gv to the module to prepair code cleanup in next
    release
    
    * Add configuration for OLED
    
    Move all the configurable parameters to configuration.txt and parse them
    in the module code
    
    * Introduce gv.cfg
    
    Change "config" to gv. cfg in OLED.py module plus minor changes to the
    configuration.txt comments

--- samplerbox.py
+++ samplerbox.py
@@ -180,6 +180,8 @@ s="config"
 AUDIO_DEVICE_ID = gv.cp.getint(s,"AUDIO_DEVICE_ID".lower())
 USE_SERIALPORT_MIDI = gv.cp.getboolean(s,"USE_SERIALPORT_MIDI".lower())
 USE_HD44780_16x2_LCD = gv.cp.getboolean(s,"USE_HD44780_16x2_LCD".lower())
+USE_OLED = gv.cp.getboolean(s,"USE_OLED".lower())
+OLED_DRIVER = gv.cp.get(s,"OLED_DRIVER".lower())
 USE_I2C_7SEGMENTDISPLAY = gv.cp.getboolean(s,"USE_I2C_7SEGMENTDISPLAY".lower())
 gv.USE_ALSA_MIXER = gv.cp.getboolean(s,"USE_ALSA_MIXER".lower())
 USE_48kHz = gv.cp.getboolean(s,"USE_48kHz".lower())
@@ -265,6 +267,14 @@ if USE_HD44780_16x2_LCD:
     lcd = lcd_16x2.HD44780()
     def display(s2,s7=""):
         lcd.display(s2,gv.basename,gv.sample_mode,gv.USE_ALSA_MIXER,gv.volume,gv.currvoice,gv.currchord,gv.chordname,gv.scalename,gv.currscale,gv.button_disp,gv.buttfunc)
+    display('Start Samplerbox')
+
+elif USE_OLED:
+	USE_GPIO=True
+    import OLED
+    oled = OLED.oled()
+    def display(s2,s7=""):
+        oled.display(s2)
     display('Start Samplerbox')
 
 elif USE_I2C_7SEGMENTDISPLAY:
