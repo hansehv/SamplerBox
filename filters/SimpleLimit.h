95566f931341c6aee21eda3415120262b6ae4b8f
hansehv <30558081+hansehv@users.noreply.github.com>
2021-02-22 23:20:49 +0100

    Doc update

--- filters/SimpleLimit.h
+++ filters/SimpleLimit.h
@@ -26,7 +26,10 @@
  *	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  *	DEALINGS IN THE SOFTWARE.
  *
- *	Adapted for samplerbox by HansEhv, https://github.com/hansehv
+ *	Downloaded from:
+ *	https://www.musicdsp.org/en/latest/Effects/204-simple-compressor-class-c.html
+ *
+ *	Adapted for samplerbox by HansEhv, https://github.com/hansehv/SamplerBox
  */
 
 
